aa0a3d7317b2dfc4584617ac02ad195d1346ce7f
Tobi Otoju <ttotoju@gmail.com>
2022-10-28 12:38:06 +0100

    Created a folder for the icons I will be making use of

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,11 +3,24 @@
 
 
 function Index() {
-    return <div>
-        <h1>My React App</h1>
-        <h1>My React App</h1>
-       <a href="https://github.com/toju-toby22/HNG-internship-Stage-1"><button>Button</button></a>
+    return <div className="back">
+                <div className="first-page">
+
+                </div>
     </div>
+
 }
 
 export default Index;
+
+
+
+
+{/* <h1>My React App</h1>
+        <h1>My React App</h1>
+        
+       <a href="https://github.com/toju-toby22/HNG-internship-Stage-1"><button>Button</button></a>
+
+       <h1>Example</h1>
+       <div title = "Specifies a space-separated list of URLs to which, when the link is followed, post requests with the body ping will be sent by the browser (in the background). Typically used for tracking.">Hover this Text to see result</div>
+    </div> */}
