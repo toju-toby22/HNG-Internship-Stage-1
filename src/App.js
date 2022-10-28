0a5813680bfa6658b4a717c56ef8d696211a944d
Tobi Otoju <ttotoju@gmail.com>
2022-10-28 02:11:18 +0100

    Added new files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import Homepage from "./pages/Homepage"
 import{BrowserRouter,Routes,Route} from 'react-router-dom'
 import './App.css';
@@ -17,3 +17,11 @@ function App() {
 }
 
 export default App;
+
+// echo "# HNG-internship-Stage-1" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/toju-toby22/HNG-internship-Stage-1.git
+// git push -u origin main
