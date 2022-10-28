ad1733802e1fc438d806db409375703b928cc48e
Tobi Otoju <ttotoju@gmail.com>
2022-10-28 01:55:01 +0100

    deploying my react app

---
+++ src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+
+
+function Index() {
+    return <div>
+        <h1>My React App</h1>
+        <h1>My React App</h1>
+       <a href="https://github.com/toju-toby22/HNG-internship-Stage-1"><button>Button</button></a>
+    </div>
+}
+
+export default Index;
